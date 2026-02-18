fd25d8bd154b63840b4c8a6375247603a252734b
Kazar Gyorgy <kazar.gyorgy@gmail.com>
2026-02-18 18:15:49 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+import Header from './components/MainNav.vue'
+</script>
+
+<template>
+  <Header />
+  <h1 class="text-6xl text-center text-orange-500 mt-6">Hello Vue</h1>
+</template>
---
+++ src/components/MainNav.vue
@@ -0,0 +1,12 @@
+<template>
+  <header class="w-full text-sm">Main Nav</header>
+</template>
+
+<script>
+export default {
+  name: 'MainNav',
+  props: {},
+  methods: {},
+  data: {},
+}
+</script>
